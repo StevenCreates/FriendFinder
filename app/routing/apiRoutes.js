be3c0df1e9f26ca6115621ab3745f8cb9d950de0
Steven Hofheins <stevenisinsane@gmail.com>
2019-11-14 08:43:21 -0700

    testing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,50 +1,42 @@
-var userData = require("../data/friends");
+var friendList = require('../data/friend.js');
 
+module.exports = function(app){
+  //a GET route that displays JSON of all possible friends
+  app.get('/api/friends', function(req,res){
+    res.json(friendList);
+  });
 
-module.exports = function(app)
-{
-    app.get("/api/friends", function(req, res)
-    {
-        res.json(userData);
-    });
+  app.post('/api/friends', function(req,res){
+    //grabs the new friend's scores to compare with friends in friendList array
+    var newFriendScores = req.body.scores;
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
 
-    app.post("/api/friends", function(req, res)
-    {
-        let bestMatchDiff = 10000;
-        let bestMatchIndex = 0;
-
-        for (let i = 0; i < userData.length; i++)
-        {
-            //console.log(userData);
-            let scoreDiff = 0;
-
-            for (let j = 0; j < userData[i].scores.length; j++)
-            {
-                scoreDiff += Math.abs(userData[i].scores[j]-req.body.scores[j]);
-                console.log(scoreDiff);
-            }
-
-            if (scoreDiff < bestMatchDiff)
-            {
-                bestMatchDiff = scoreDiff;
-                bestMatchIndex = i;
-            }
-        }
-
-        console.log(userData[bestMatchIndex]);
-
-        userData.push(req.body);
-        res.json(userData[bestMatchIndex]);
-    });
-
-    app.get("/api/clear", function(req, res)
-    {
-      while (userData.length !== 1)
-      {
-        userData.pop();
+    //runs through all current friends in list
+    for(var i=0; i<friendList.length; i++){
+      var scoresDiff = 0;
+      //run through scores to compare friends
+      for(var j=0; j<newFriendScores.length; j++){
+        scoresDiff += (Math.abs(parseInt(friendList[i].scores[j]) - parseInt(newFriendScores[j])));
       }
-      
-      //res.json({ ok: true });
-      res.redirect("/");
-    });
+
+      //push results into scoresArray
+      scoresArray.push(scoresDiff);
+    }
+
+    //after all friends are compared, find best match
+    for(var i=0; i<scoresArray.length; i++){
+      if(scoresArray[i] <= scoresArray[bestMatch]){
+        bestMatch = i;
+      }
+    }
+
+    //return bestMatch data
+    var bff = friendList[bestMatch];
+    res.json(bff);
+
+    //pushes new submission into the friendsList array
+    friendList.push(req.body);
+  });
 };
