f531b27df078676c29b647438e54b627c50bc137
Steven Hofheins <stevenisinsane@gmail.com>
2019-11-14 08:23:18 -0700

    working on functionality

--- app/data/friends.js
+++ app/data/friends.js
@@ -17,3 +17,5 @@ var friend = {
         1
       ]
   }
+
+  module.exports = userArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,9 +2,15 @@ const express = require("express");
 const handlebr = require("handlebars");
 const app = express();
 
-app.all('*',(req, res)=>{
+app.all('/',(req, res)=>{
 
     res.send('<h1>this is a test</h1>')
 });
+
+app.use(express.static('public'))
+
+// app.get('/',(req, res)=>{
+
+// });
 
 app.listen(3000);
